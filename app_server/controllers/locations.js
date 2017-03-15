c2eee9a2b5f2feb3015e4b29c32c6e2ce33808a6
Rachel Fuller <rlfuller@gmail.com>
2017-03-15 11:54:01 -0400

    add post for reviews

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -20,6 +20,36 @@ var renderHomePage = function(req, res, responseBody){
     });
 };
 
+var getLocationInfo = function(req, res, callback){
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {}
+  };
+
+  request(requestOptions, function(err, response, body){
+    if(response.statusCode === 200) {
+      body.coords = {
+        lng: body.coords[0],
+        lat: body.coords[1]
+      };
+      //console.log("created on: " + body.reviews[0].createdOn);
+      body.reviews.forEach(function(review){
+        review.createdOn = moment(review.createdOn).format("MMMM Do YYYY, h:mm a");
+      });
+
+      callback(req, res, body);
+    } else {
+      _showError(req, res, response.statusCode);
+    }
+
+  });
+
+};
+
 var renderDetailPage = function(req, res, locDetail){
   res.render("location-info", {
       title: locDetail.name,
@@ -31,6 +61,23 @@ var renderDetailPage = function(req, res, locDetail){
 
   });
 
+};
+
+var renderReviewForm = function(req, res, locDetail){
+  res.render("location-review-form", {
+      title: "Review " + locDetail.name + " on WIFInder",
+      pageHeader: {title: "Review " + locDetail.name}
+  });
+};
+
+var _showError = function(req, res, status){
+  var title, content;
+
+  title = status  + ": Something, somewhere, has gone just a little bit wrong.";
+  content = "We can't find what you are looking for. Sorry.";
+
+  res.status(status);
+  res.render("error", {title: title, content: content});
 };
 
 /* GET 'home' page */
@@ -67,31 +114,41 @@ module.exports.homelist = function(req, res){
 
 /* GET 'detail' page */
 module.exports.locationInfo = function(req, res){
-  var requestOptions, path;
-  path = "/api/locations/" + req.params.locationid;
-  requestOptions = {
-    url: apiOptions.server + path,
-    method: "GET",
-    json: {}
-  };
-  request(requestOptions, function(err, response, body){
-    body.coords = {
-      lng: body.coords[0],
-      lat: body.coords[1]
-    };
-    //console.log("created on: " + body.reviews[0].createdOn);
-    body.reviews.forEach(function(review){
-      review.createdOn = moment(review.createdOn).format("MMMM Do YYYY, h:mm a");
-    });
-
-    renderDetailPage(req, res, body);
+  getLocationInfo(req, res, function(req, res, responseData){
+    renderDetailPage(req, res, responseData);
   });
 };
 
 /* GET 'add a new review' page */
 module.exports.addReview = function(req, res){
-    res.render("location-review-form", {
-        title: "Review Starbucks on WIFInder",
-        pageHeader: {title: "Review Starbucks"}
-    });
+  getLocationInfo(req, res, function(req, res, responseData){
+    renderReviewForm(req, res, responseData);
+  });
+};
+
+/* POST add a new review */
+module.exports.doAddReview = function(req, res){
+  var requestOptions, path, locationid, postdata;
+  locationid = req.params.locationid;
+  path = "/api/locations/" + locationid + "/reviews";
+  postdata = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "POST",
+    json: postdata
+  };
+
+  request(requestOptions, function(err, response, body){
+    if(response.statusCode === 201){
+      res.redirect("/location/" + locationid);
+    } else {
+      _showError(req, res, response.statusCode);
+    }
+  });
+
 };
